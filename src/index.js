89f7910932b8dfef4543216451b84b8c55528108
iamNitin16 <nitinsharma95ns@gmail.com>
2019-02-17 23:38:29 +0530

    Added more query with data in main memory

--- src/index.js
+++ src/index.js
@@ -3,12 +3,33 @@ const { GraphQLServer } = require("graphql-yoga");
 const typeDefs = `
   type Query {
     info: String!
+    feed: [Link!]!
+  }
+
+  type Link {
+    id: ID!
+    description: String!
+    url: String!
   }
 `
 
+let links = [{
+  id: 'link-0',
+  url: 'https://howtographql.com',
+  description: "Fullstack Tutorial for GraphQL"
+}]
+
 const resolvers = {
   Query: {
-    info: () => `This is the API of a Hackernews clone`
+    info: () => `This is the API of a Hackernews clone`,
+
+    feed: () => links,
+  },
+
+  Link: {
+    id: (parent) => parent.id,
+    description: (parent) => parent.description,
+    url: (parent) => parent.description,
   }
 }
 
